64b96a22fd05794d3f25bef2397bb24aa5b36482
Mahedi Hasan Niloy <139734574+hpl-sd-niloy@users.noreply.github.com>
2024-03-26 15:57:43 +0600

    Add project files.

---
+++ TalentTrack.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TalentTrack.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
